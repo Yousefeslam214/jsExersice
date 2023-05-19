efbe42ac4e19e09a26beb96cf5fcebbf6c520194
yousefeslam <email@example.com>
2023-05-20 01:24:36 +0200

    make clock

--- js/main.js
+++ js/main.js
@@ -156,4 +156,38 @@ myContent.onkeyup = function () {
 }
 
 
+//to stop rigth click
+
+// document.addEventListener('contextmenu',function(e) {
+//        e.preventDefault()
+// })
+
+
+// clock
+function showTime() {
+       var now = new Date(),
+       hours = now.getHours(),
+       minutes = now.getMinutes(),
+       seconds = now.getSeconds()
+       if (hours < 10) {
+              hours = '0' + hours;
+       }
+       if (minutes < 10) {
+              minutes = '0' + minutes;
+       }
+       if (seconds < 10) {
+              seconds = '0' + seconds;
+       }
+       document.getElementById('clock').textContent = hours +":"+minutes +":"+seconds ;
+
+}
+
+
+window.onload = function () {
+       setInterval(showTime, 500)
+}
+
+
+
+
 
